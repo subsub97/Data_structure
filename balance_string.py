c4243f1c53e68b405303f5af3cb6518f403cec78
Ho <godqhr721@gmail.com>
2022-04-28 12:32:18 +0900

    수정중입니다

--- balance_string.py
+++ balance_string.py
@@ -1,19 +1,25 @@
 def solve(A):
-    if (A.count('0') == A.count('1')):
-        return (len(A))
+    if(A.count('0') == A.count('1')):
+        return(len(A))
     else:
-        if (A.count("0") > A.count("1")):
-            Gap = A.count("1") * 2 + 1
-        else:
-            Gap = A.count("0") * 2 + 1
+        n = len(A)
+        count = 0
+        start = 0
+        end = n
+        a = 0
+        while count != n:
 
-    for i in range(0, len(A) - 1):
-        for j in range(0, i + 1):
-            B = A[j:j + Gap]
-            if (B.count("0") == B.count("1")):
-                same = B.count("0") * 2
-                return same
-        Gap -= 1
+            count +=2
+            a -=2
+
+            if count == n:
+                count = 0
+                start += 1
+                end -= 1
+                n -= 2
+                a=0
+        print("end")
+
 
 
 A = input().strip()
