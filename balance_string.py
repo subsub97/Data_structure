7eb0ee87dc464e3e81ba894d41648d82fd8cae8d
Ho <godqhr721@gmail.com>
2022-05-02 14:17:03 +0900

    3차 수정중

--- balance_string.py
+++ balance_string.py
@@ -1,24 +1,42 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    def push(self,val):
+        self.items.append(val)
+    def pop(self):
+        try:
+            return self.items.pop()
+        except IndexError:
+           print("dd")
+    def top(self):
+       return self.items[-1]
+
+    def __len__(self):
+        return len(self.items)
+    def isEmpty(self):
+        return self.__len__() ==0
+
 def solve(A):
+    count = 0
+    point = 0
     if(A.count('0') == A.count('1')):
         return(len(A))
     else:
-        n = len(A)
-        count = 0
-        start = 0
-        end = n
-        a = 0
-        while count != n:
+        S = Stack()
+        S.push(None)
+        for i in range(0,len(A)):
+            if S.items[-1] == None:
+                S.push(int(A[i]))
+            elif S.top() == int(A[i]):
+                S.push(int(A[i]))
+            else:
+                S.pop()
+                point += 1
+                count +=1
+                if count >= 1
 
-            count +=2
-            a -=2
-
-            if count == n:
-                count = 0
-                start += 1
-                end -= 1
-                n -= 2
-                a=0
-        print("end")
+        print(len(S))
+        print(count*2)
 
 
 
